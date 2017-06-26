3acfd73d1680258c1d4c9e119f6715d9b972c211
Undefined <Undefined@undefined.local>
2017-06-25 23:33:39 -0400

    Working return of string

--- jnilink_cproject/src/AnswerHelloCVersion.c
+++ jnilink_cproject/src/AnswerHelloCVersion.c
@@ -1,10 +1,26 @@
 #include <jni.h>
- #include <stdio.h>
- #include "com_lleclerc_jnilink_SayHelloMain.h"
-  
- JNIEXPORT void JNICALL 
- Java_HelloWorld_print(JNIEnv *env, jobject obj)
- {
-     printf("Hello World!\n");
-     return;
- }
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "com_lleclerc_jnilink_SayHelloMain.h"
+
+JNIEXPORT jstring JNICALL Java_com_lleclerc_jnilink_SayHelloMain_sayHello(
+		JNIEnv *env, jobject obj, jstring string) {
+// Return what I get
+//	return string;
+
+// Return new string
+//	jstring jstr = NULL;
+//	jstr= (*env)->NewStringUTF(env, "Abc");
+//	return jstr;
+
+// Modify the string
+	const char *name = (*env)->GetStringUTFChars(env, string, NULL);
+	char msg[60] = "Hello ";
+	jstring result;
+
+	strcat(msg, name);
+	(*env)->ReleaseStringUTFChars(env, string, name);
+	result = (*env)->NewStringUTF(env, msg);
+	return result;
+}
